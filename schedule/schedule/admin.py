29f22a3cf0b74ece5834f1841a53c985be6a7d68
Maxim Beiner <mbeiner@crystalnix.com>
2017-02-12 22:26:17 +0600

    Add Discipline and Theme models to admin site.

--- schedule/schedule/admin.py
+++ schedule/schedule/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Specialty, Troop
+from .models import Specialty, Troop, Discipline, Theme
 
 admin.site.register(Specialty)
 admin.site.register(Troop)
+admin.site.register(Discipline)
+admin.site.register(Theme)
